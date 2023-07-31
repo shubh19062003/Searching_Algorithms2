f69c50eb044046f85736d5558e3155ae0c417ac1
shubh19062003 <shubhamrajverma.5060@gmail.com>
2023-07-31 23:33:30 +0530

    q1365 leetcode

--- arraysquesleet.java
+++ arraysquesleet.java
@@ -92,20 +92,45 @@ public class arraysquesleet {
          * kids.
          */
 
-        int[] candies = { 2, 3, 5, 1, 3 };
-        int extraCandies = 3;
-        ArrayList<Boolean> list = new ArrayList<Boolean>(candies.length);
-        int max = Arrays.stream(candies).max().getAsInt();
-        for (int i = 0; i < candies.length; i++) {
-            if (candies[i] + extraCandies >= max) {
-                list.add(true);
-            } else {
-                list.add(false);
+        // int[] candies = { 2, 3, 5, 1, 3 };
+        // int extraCandies = 3;
+        // ArrayList<Boolean> list = new ArrayList<Boolean>(candies.length);
+        // int max = Arrays.stream(candies).max().getAsInt();
+        // for (int i = 0; i < candies.length; i++) {
+        // if (candies[i] + extraCandies >= max) {
+        // list.add(true);
+        // } else {
+        // list.add(false);
+        // }
+
+        // }
+        // System.out.println(list);
+
+        // // // 1365. How Many Numbers Are Smaller Than the Current Number
+        /*
+         * Input: nums = [8,1,2,2,3]
+         * Output: [4,0,1,1,3]
+         * Explanation:
+         * For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+         * For nums[1]=1 does not exist any smaller number than it.
+         * For nums[2]=2 there exist one smaller number than it (1).
+         * For nums[3]=2 there exist one smaller number than it (1).
+         * For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+         */
+
+        int[] nums = { 6, 5, 4, 8 };
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    count++;
+
+                }
             }
-
+            ans[i] = count;
         }
-        System.out.println(list);
-
+        System.out.println(Arrays.toString(ans));
     }
 
 }
